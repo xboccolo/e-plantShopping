0b6623aaa0b6de2957eee71f41d8343ee9d412c6
Your Name <you@example.com>
2026-06-24 18:38:12 +0200

    Deploying to gh pages

--- src/store.js
+++ src/store.js
@@ -6,3 +6,10 @@ import cartReducer from './CartSlice';
     },
 });
 export default store
+
+//la parola cartReducer l'ho inventata qui nel moemnto dell'import
+//dentro CartSlice esporto CartSlice.reducer
+// L'oggetto reducer definisce la struttura di tutta la tua applicazione.
+// Scrivendo cart: cartReducer, stai creando un reparto ufficiale chiamato 
+// cart all'interno dello stato globale, e stai dicendo che la gestione 
+// di quel reparto è affidata esclusivamente alle regole scritte in cartReducer.
